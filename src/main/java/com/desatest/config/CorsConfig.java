24ecc072a09026a3992c342bea2977d19e7a0195
Juan Montesinos <juanmontesinos220803@gmail.com>
2026-06-03 11:33:40 -0500

    NUEVO CORS

--- src/main/java/com/desatest/config/CorsConfig.java
+++ src/main/java/com/desatest/config/CorsConfig.java
@@ -14,9 +14,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://desafront.netlify.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
